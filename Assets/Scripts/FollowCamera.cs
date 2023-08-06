c326588cd2bf06b1a9f2f4bb799ea561f847dc0c
Yom3n <kamilbudnik94@gmail.com>
2023-08-06 15:40:23 +0200

    Follow Camera

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] private GameObject target;
+    private Vector3 vectorToTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vectorToTarget = transform.position - target.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.transform.position + vectorToTarget;
+    }
+}
